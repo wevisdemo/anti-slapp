75bdc692018f8001a3642b910cd0d5b4210e0b36
Pimpatipan <Pimpatipan@users.noreply.github.com>
2022-03-22 06:58:12 +0000

    deploy: 95609044ba8930ad647f16f262181c8969f48602

--- _nuxt/static/1647931932/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fanti-slapp\u002F_nuxt\u002Fstatic\u002F1647931932",layout:"default",error:a,state:{score:0,warnUpAns:b,soundOn:b,isClickFromMenu:c},serverRendered:c,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fanti-slapp\u002F",assetsPath:"\u002Fanti-slapp\u002F_nuxt\u002F",cdnURL:a}}}}(null,false,true));
